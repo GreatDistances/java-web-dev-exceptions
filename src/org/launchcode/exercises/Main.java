77a37bb2c93c4f3359d978139d11e3073c0f10f2
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-02-07 14:06:39 -0600

    11.1

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,7 +1,6 @@
 package org.launchcode.exercises;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
@@ -26,6 +25,7 @@ public class Main {
     public static int CheckFileExtension(String fileName)
     {
         // Write code here!
+        return 0;
     }
 
 }
